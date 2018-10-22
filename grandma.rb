178e85fe2ba304469c15a0c0179ee98eb1f8cbda
John Martinez <jmartinez729@gmail.com>
2018-10-22 16:11:54 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -15,10 +15,9 @@
 def speak_to_grandma(string)
   if string != string.upcase
     puts "HUH?! SPEAK UP, SONNY!"
-  else
-    puts "NO, NOT SINCE 1938!"
-    
   elsif string == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
+  else
+    puts "NO, NOT SINCE 1938!"
   end 
 end 
